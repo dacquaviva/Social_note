e19a1e65acc1fc4b4c28f32f5f67623b2eb6162d
Daniele Acquaviva <danieleacquaviva5@gmail.com>
2018-05-10 20:01:02 +0200

    Modified root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :notes
-  root to: "users#index"
+  root to: "notes#index"
   devise_for :users
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
