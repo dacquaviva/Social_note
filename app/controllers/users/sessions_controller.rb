7500d94f573dd994b8f60fa95646f75ffc96a398
Daniele Acquaviva <danieleacquaviva5@gmail.com>
2018-05-22 06:16:15 +0200

    Added user routes

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,31 +1,7 @@
-# frozen_string_literal: true
-
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 before_action :set_user, only: [:show, :edit, :update, :destroy,:addfriend]
-
+before_action :authenticate_user!
   # GET /users
   # GET /users.json
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
     get 'notes/:id/users', to: 'notes#newsharing' ,as: 'new_sharing_note'
   post 'notes/:id/users', to: 'notes#createsharing' ,as: 'create_sharing_note'
+  post 'users/:id/add', to: 'users#addfriend' ,as: 'add_friend'
   resources :notecollections
   resources :notes
   root to: "notes#index"
-  devise_for :users, controllers: { sessions: 'users/sessions' }
-  resources :users
-
+  devise_for :users
+    as :user do
+       get '/users', to: 'users/sessions#index'
+        get '/users/:id', to: 'users/sessions#show' , as: "user"
+        get '/users/:id/edit', to: 'users/sessions#edit' , as: "edit_user"
+        get '/users/new', to: 'users/sessions#new' , as: "new_user"
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
